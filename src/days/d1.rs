43781a2223d8f01c0a6201866a4eb308156f68a1
Erik Holzinger <erik.holzinger@hs-augsburg.de>
2021-12-03 13:28:59 +0100

    Refactor unnecessary code

--- src/days/d1.rs
+++ src/days/d1.rs
@@ -8,8 +8,7 @@ pub fn count_larger_measurements() -> u32 {
         .collect();
     let mut counter: u32 = 0;
     let _ = array.windows(2)
-        .inspect(|w| if w[1] > w[0] {counter += 1})
-        .collect::<Vec<_>>();
+        .inspect(|w| if w[1] > w[0] {counter += 1});
     counter
 }
 
@@ -22,7 +21,6 @@ pub fn count_larger_measurements_windows() -> u32 {
     let mut counter: u32 = 0;
     let _ = array.windows(3)
         .zip(array.windows(3).skip(1))
-        .inspect(|(w0, w1)| if w1.iter().sum::<u32>() > w0.iter().sum::<u32>() {counter += 1})
-        .collect::<Vec<_>>();
+        .inspect(|(w0, w1)| if w1.iter().sum::<u32>() > w0.iter().sum::<u32>() {counter += 1});
     counter
 }
